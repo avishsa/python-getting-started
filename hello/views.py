18a2bbc7ad9fcd072d30f89a21c4942a9bfd120a
avishsa <avishag1991@gmail.com>
2021-03-17 11:51:35 +0200

    My first commit

--- hello/views.py
+++ hello/views.py
@@ -4,16 +4,9 @@ from django.http import HttpResponse
 from .models import Greeting
 
 # Create your views here.
-def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+def data(request):
+    return HttpResponse(json.dumps({'data':1}), content_type="application/json")
+    #return render(request, "index.html")
 
 
-def db(request):
 
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
