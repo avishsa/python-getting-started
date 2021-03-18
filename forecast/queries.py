8f313ab82c5276794af483516873ec3a5ae04bdd
avishsa <avishag1991@gmail.com>
2021-03-18 21:56:07 +0200

    change f

--- forecast/queries.py
+++ forecast/queries.py
@@ -25,7 +25,7 @@ def get_sum(lon,lat):
     fc_qs = list(Forecast.objects.filter(lon=lon,lat=lat))
     if(len(list(fc_qs)) == 0):
         fcs_qs = Forecast.objects.filter(lon=lon,lat=lat)
-        if(len(list(fcs)) ==0):
+        if(len(list(fcs_qs)) ==0):
             return {'msg':'not found location'}
         temp_max = fcs_qs.aggregate(Max('Temperature'))["Temperature__max"]
         temp_min = fcs_qs.aggregate(Min('Temperature'))["Temperature__min"]
