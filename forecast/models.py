97ee462b5f4bc33159ea236326cbc0a62d684ea2
avishsa <avishag1991@gmail.com>
2021-03-18 10:22:14 +0200

    change decimal to float

--- forecast/models.py
+++ forecast/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Forecast(models.Model):
-    lon = models.DecimalField(max_digits=5, decimal_places=2,verbose_name="lon")
-    lat = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="lat")
+    lon = models.FloatField(verbose_name="lon")
+    lat = models.FloatField(verbose_name="lat")
     forecast_time = models.DateTimeField(verbose_name="forecast time")
-    temperature = models.FloatField(max_digits=5, decimal_places=2,verbose_name="temperature") 
-    precipitation = models.FloatField(max_digits=5, decimal_places=2, verbose_name="precipitation") 
+    temperature = models.FloatField(verbose_name="temperature") 
+    precipitation = models.FloatField(verbose_name="precipitation") 
